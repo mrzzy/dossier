2027be3ed9cfbc301f9deaa6b70c6f148a5b556e
Zhu Zhan Yan <program.nom@gmail.com>
2018-11-27 15:05:41 +0800

    Added base landing page content and styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,22 +6,21 @@
 import React from 'react';
 
 import "../styles/LandingPage.css";
-import DevImage from "../assets/landing_laptop.jpg";
+import HeroImage from "../assets/landing_laptop_clipped.jpg";
 
 /* Landing Page component displays the landing page */
 function LandingPage() {
     return (
-        <section id="landing" class="page">
-            <div class="container">
-                <div class="tagline">
-                    <h1 class="fancy"> making and breaking. </h1>
-                    est. 2014 
+        <section id="landing" class="page container">
+            <div class="tagline">
+                <div class="container">
+                    <h1 class="fancy"> making and breaking.  </h1>
+                    <span class="footnote">est. 2014</span>
                 </div>
             </div>
-            <img src={DevImage} alt="software development"/>
+            <img class="hero" src={HeroImage} alt="software development"/>
         </section>
     );
-
 }
 
 export default LandingPage;
