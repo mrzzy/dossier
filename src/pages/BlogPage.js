5cde4f6bc2f06f30981417949d78ee643920c4ab
Zhu Zhan Yan <program.nom@gmail.com>
2018-11-28 14:31:23 +0800

    Added skeleton structure and styling for BlogPage

---
+++ src/pages/BlogPage.js
@@ -0,0 +1,19 @@
+/*
+ * pages/BlogPage.js
+ * Blog Page
+*/
+
+import React from "react"
+
+import "../styles/BlogPage.css";
+
+/* Blog Page renders the blog section of the site */
+function BlogPage(props) {
+    return (
+        <section Class="page" id="blog">
+            <h1 Class="fancy">writing.</h1>
+        </section>
+    );
+}
+
+export default BlogPage;
