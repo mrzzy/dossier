e29b0b01bfa2bd006d43fe38b278b104dc0250a2
Zhu Zhan Yan <program.nom@gmail.com>
2018-11-29 11:00:06 +0800

    Added BlogList comoponent with AJAX to retrieve blog listing

---
+++ src/components/BlogListing.js
@@ -0,0 +1,76 @@
+/*
+ * components/BlogListing.js
+ * Blog Listing Components
+*/
+
+import React from 'react';
+
+/* Blog entry component represents a entry in a Blog List.
+ * The title is given by a title prop, the summary given by the summary prop.
+*/
+function BlogEntry(props) {
+    return (
+        <div className="blog-entry">
+            <h2 className="title">{props.title}</h2>
+            <span className="summary">{props.summary}</span>
+        </div>
+    );
+}
+
+
+/* Blog List component display a listing of blog posts based on the 
+ * blog /content/blog/manifest.json
+*/
+class BlogList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            manifest: null
+        }
+
+        // Bind callbacks
+        this.loadContents = this.loadContents.bind(this);
+    }
+
+    /* Load blog manifest with contents and trigger rerender with contents */
+    loadContents() {
+        fetch("/content/blog/manifest.json")
+            .then((response) => response.json())
+            .then((manifest) => {
+                // trigger render with manifest infomation
+                this.setState({
+                    manifest: manifest,
+                });
+            });
+    }
+
+    /* Renders loading display if manifest has not yet loaded,
+     * otherwise, displays the blog listing from the data in the loaded manifest
+    */
+    render() {
+        if(this.state.manifest === null) {
+            // Contents not loaded, render loading display
+            console.log("render without manifest...");
+            return (
+                <div className="blog-list">
+                    <span className="loading">Loading&hellip;</span>
+                </div>
+            );
+        } else {
+            // Contents has loaded, render blog posts from manifest
+            console.log("render with manifest...");
+        
+            return null;
+        }
+                
+    }
+
+    /* Lifecycle events */
+    componentDidMount() {
+        this.loadContents();
+    }
+}
+
+
+export default BlogList;
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -4,14 +4,16 @@
 */
 
 import React from "react"
+import BlogList from "../components/BlogListing.js";
 
 import "../styles/BlogPage.css";
 
 /* Blog Page renders the blog section of the site */
 function BlogPage(props) {
     return (
-        <section Class="page" id="blog">
-            <h1 Class="fancy">writing.</h1>
+        <section className="page" id="blog">
+            <h1 className="fancy">writing.</h1>
+            <BlogList/>
         </section>
     );
 }
