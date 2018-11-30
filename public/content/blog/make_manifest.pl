f2037247fba2889d530353d6e9a581f20a04e438
Zhu Zhan Yan <program.nom@gmail.com>
2018-11-30 11:46:48 +0800

    Added table of contents generation to manifest

--- public/content/blog/make_manifest.pl
+++ public/content/blog/make_manifest.pl
@@ -2,7 +2,8 @@
 # gen_manifest.pl
 # Dossier
 # Generate Blog manifest
-# Manifest is used by the site to discover and display available blogs
+# Manifest is used by the site to discover and obtain metadata about available
+# entrys
 #
 
 use warnings;
@@ -12,6 +13,7 @@ use POSIX qw(strftime);
 
 use JSON::PP;
 
+my $BLOG_PATH = '/content/blog';
 my $TITLE_REGEX = qr/([-\w?,.\/!:() ]+)/;
  
 ## Utility Functionality
@@ -55,36 +57,40 @@ sub extract_contents_table
     return \@contents_table;
 }
 
-# Extract metadata for the post at the given path
+# Extract metadata for the blog entry at the given path
 # Returns the extracted metadata as a referene to hash
 sub extract_metadata 
 {
     my ($path) = @_;
 
-    # Determine post timestamp
+    # Determine entry timestamp
     my $mod_time = (stat($path))[9];
     my $timestamp =  &convert_iso_date($mod_time);
     
-    # Read post content
-    open(my $post, '<', $path);
-    my @contents = <$post>;
+    # Read entry content
+    open(my $entry, '<', $path);
+    my @contents = <$entry>;
 
-    # Extract post title
+    # Extract entry title
     my $title = $contents[0];
     chomp $title;
     $title =~ s/\W*${TITLE_REGEX}\W*/$1/g;
     
-    # Extract post subtitle
+    # Extract entry subtitle
     my $subtitle = $contents[1];
     chomp $subtitle;
     $subtitle =~ s/\W*${TITLE_REGEX}\W*/$1/g;
     
-    # Extract post table of contnts
+    # Extract entry table of contnts
     my $contents_table = &extract_contents_table(\@contents);
 
-    # Extract post identifier from path
+    # Extract entry identifier from path
     my $id = $path;
     $id =~ s/\W*(\w+).*/$1/g;
+    
+    # Extract href path from path
+    my $href = $path;
+    $href =~ s/(.*)\.md$/${BLOG_PATH}\/$1\.pd/;
     
     # Build metadata, to manifest
     my $metadata = {
@@ -92,21 +98,22 @@ sub extract_metadata
         "timestamp" => $timestamp,
         "title" => $title,
         "subtitle" => $subtitle,
+        "href" => $href,
         "contents_table" => $contents_table
     };
-        
+
     return $metadata;
 }
 
-
-# Build manifest
+# Define entrys as all markdown files
+my @entry_paths = <*.md>;
 my @manifest = ();
-# Define posts as all markdown files
-my @post_paths = <*.md>;
-for my $path (@post_paths) {
+for my $path (@entry_paths) {
+    # Build manifest
     my $metadata = &extract_metadata($path);
     push @manifest, $metadata;
 }
+
 
 # Output manifest to file 
 my $manifest_json = encode_json \@manifest;
